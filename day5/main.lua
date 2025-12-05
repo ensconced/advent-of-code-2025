ebf90056039eb49fa8eee4437587cd3c210b2805
Joe Barnett <joe.barnett@elliptic.co>
2025-12-05 20:36:35 +0000

    day5 part 2 :)

--- day5/main.lua
+++ day5/main.lua
@@ -15,14 +15,12 @@ function Range:size()
   return self.upper - self.lower
 end
 
-function Range:disjoint(other)
-  return other.upper < self.lower or other.lower > self.upper
+function Range:is_mergeable_with(other)
+  return self:includes(other.lower) or self:includes(other.upper) or other:includes(self.lower) or
+      other:includes(self.upper)
 end
 
 function Range:merge(other)
-  if self:disjoint(other) then
-    error("tried to merge disjoint ranges")
-  end
   return Range:new(math.min(self.lower, other.lower), math.max(self.upper, other.upper))
 end
 
@@ -32,7 +30,8 @@ local function parse_input(input_path)
   for line in io.lines(input_path) do
     local range_start, range_end = line:match("^(%d+)-(%d+)$")
     if range_start and range_end then
-      table.insert(ranges, Range:new(tonumber(range_start), tonumber(range_end) + 1))
+      local range = Range:new(tonumber(range_start), tonumber(range_end) + 1)
+      table.insert(ranges, range)
     elseif line:match("^%d+$") then
       table.insert(ids, tonumber(line))
     end
@@ -40,6 +39,24 @@ local function parse_input(input_path)
   return { ranges = ranges, ids = ids }
 end
 
+local function merge_ranges(ranges)
+  local i = 1
+  while i <= #ranges do
+    local range = ranges[i]
+    local j = i + 1
+    while j <= #ranges do
+      local merge_candidate = ranges[j]
+      if range:is_mergeable_with(merge_candidate) then
+        ranges[j] = ranges[i]:merge(ranges[j])
+        table.remove(ranges, i)
+        return true
+      end
+      j = j + 1
+    end
+    i = i + 1
+  end
+  return false
+end
 
 local function part1(input_path)
   local parsed = parse_input(input_path)
@@ -55,17 +72,20 @@ local function part1(input_path)
   return count
 end
 
-
 local function part2(input_path)
   local parsed = parse_input(input_path)
-  local merged = Range:new(0, 0)
+
+  -- merge ranges until all the remaining ones are disjoint
+  while merge_ranges(parsed.ranges) do end
+
+  local sum = 0
   for _, range in pairs(parsed.ranges) do
-    print(merged.lower, merged.upper, range.lower, range.upper, merged:merge(range).lower, merged:merge(range).upper)
-    merged = merged:merge(range)
+    sum = sum + range:size()
   end
-  return merged:size()
+  return sum
 end
 
 assert(part1("./day5/example-input.txt") == 3)
 assert(part1("./day5/input.txt") == 744)
 assert(part2("./day5/example-input.txt") == 14)
+assert(part2("./day5/input.txt") == 347468726696961)
