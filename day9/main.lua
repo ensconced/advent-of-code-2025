d141e33b8aec3481e00ea34d9c410279cca0d157
Joe Barnett <joe.barnett@elliptic.co>
2025-12-10 04:39:17 +0000

    day9 part 1

---
+++ day9/main.lua
@@ -0,0 +1,24 @@
+local function parse_input(input_path)
+  local nodes = {}
+  for line in io.lines(input_path) do
+    local x, y = line:match("^(%d+),(%d+)$")
+    table.insert(nodes, { x = x, y = y })
+  end
+  return nodes
+end
+
+local function part1(input_path)
+  local nodes = parse_input(input_path)
+
+  local max = 0
+  for _, node_a in pairs(nodes) do
+    for _, node_b in pairs(nodes) do
+      max = math.max(max, (math.abs(node_a.x - node_b.x) + 1) * (math.abs(node_a.y - node_b.y) + 1))
+    end
+  end
+  return max
+end
+
+
+assert(part1("./day9/example-input.txt") == 50)
+assert(part1("./day9/input.txt") == 4781235324)
