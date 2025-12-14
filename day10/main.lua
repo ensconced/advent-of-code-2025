09b081f0b189d21634814d9446484c64a8abed55
Joe Barnett <joe.barnett@elliptic.co>
2025-12-14 21:00:50 +0000

    wip: day10 part2

--- day10/main.lua
+++ day10/main.lua
@@ -9,23 +9,35 @@ end
 local function parse_buttons(buttons_str)
   local buttons = {}
   for button_str in buttons_str:gmatch("%([%d,]+%)") do
-    local button = 0
+    local button = { list = {}, mask = 0 }
     for num_str in button_str:gmatch("%d+") do
-      button = button | 1 << tonumber(num_str)
+      local num = tonumber(num_str)
+      table.insert(button.list, num)
+      button.mask = button.mask | 1 << num
     end
     table.insert(buttons, button)
   end
   return buttons
 end
 
+local function parse_joltages(joltages_str)
+  local joltages = {}
+  for joltage in joltages_str:gmatch("%d+") do
+    table.insert(joltages, tonumber(joltage))
+  end
+  return joltages
+end
+
 local function parse_input(input_path)
   local machines = {}
   for line in io.lines(input_path) do
-    local machine = { buttons = {} }
-    local indicator_str, buttons_str = line:match("^%[([.#]+)%] ([%(%)%d, ]+)")
-    machine.indicators = parse_indicators(indicator_str)
-    machine.buttons = parse_buttons(buttons_str)
-    table.insert(machines, machine)
+    local indicator_str, buttons_str, joltages_str = line:match("^%[([.#]+)%] ([%(%)%d, ]+) {([%d,]+)}")
+    table.insert(machines,
+      {
+        indicators = parse_indicators(indicator_str),
+        buttons = parse_buttons(buttons_str),
+        joltages = parse_joltages(joltages_str)
+      })
   end
   return machines
 end
@@ -54,7 +66,7 @@ end
 local function is_valid_button_combination(machine, button_combination)
   local indicators = 0
   for _, button in pairs(button_combination) do
-    indicators = indicators ~ button
+    indicators = indicators ~ button.mask
   end
   return indicators == machine.indicators
 end
@@ -81,5 +93,98 @@ local function part1(input_path)
   return total
 end
 
+local function serialise_num_list(list)
+  local str_arr = {}
+  for _, num in pairs(list) do
+    table.insert(str_arr, tostring(num))
+  end
+  return string.format("[ %s ]", table.concat(str_arr, ", "))
+end
+
+
+local function copy_list(list)
+  local copy = {}
+  for _, val in pairs(list) do
+    table.insert(copy, val)
+  end
+  return copy
+end
+
+local function min_presses_to_joltage(machine)
+  local function branch_lower_bound(current_joltages, current_press_count)
+    local remaining_presses_lower_bound = 0
+    for i, target_joltage in pairs(machine.joltages) do
+      remaining_presses_lower_bound = remaining_presses_lower_bound + target_joltage - current_joltages[i]
+    end
+    return remaining_presses_lower_bound + current_press_count
+  end
+
+  local function check_joltage_status(current_joltages)
+    local done = true
+    for i, joltage in pairs(current_joltages) do
+      if joltage > machine.joltages[i] then
+        return false, true
+      elseif joltage ~= machine.joltages[i] then
+        done = false
+      end
+    end
+    return done, false
+  end
+
+  local function find_min_for_branch(current_press_count, current_joltages, min_press_count)
+    local indent = ("  "):rep(current_press_count)
+    print(string.format("%scurrent_press_count: %d", indent, current_press_count))
+    print(string.format("%smin_press_count: %d", indent, min_press_count))
+    print(string.format("%scurrent_joltages: %s", indent, serialise_num_list(current_joltages)))
+    local reached_target_joltages, exceeded_target_joltages = check_joltage_status(current_joltages)
+
+    if reached_target_joltages then
+      print(string.format("%sreached target", indent))
+      return math.min(current_press_count, min_press_count)
+    end
+
+    if branch_lower_bound(current_joltages, current_press_count) >= min_press_count then
+      print(string.format("%spruning by lower bound", indent))
+      return min_press_count
+    end
+
+    if exceeded_target_joltages then
+      print(string.format("%sexceeded joltage targets", indent))
+      return min_press_count
+    end
+
+    for _, button in pairs(machine.buttons) do
+      local next_press_count = current_press_count + 1
+      local next_joltages = copy_list(current_joltages)
+      for _, joltage_idx in pairs(button.list) do
+        next_joltages[joltage_idx + 1] = next_joltages[joltage_idx + 1] + 1
+      end
+      print(string.format("%snext_joltages: %s", indent, serialise_num_list(next_joltages)))
+      min_press_count = math.min(min_press_count, find_min_for_branch(next_press_count, next_joltages, min_press_count))
+    end
+
+    return min_press_count
+  end
+
+  local initial_joltages = {}
+  for _ = 1, #machine.joltages do
+    table.insert(initial_joltages, 0)
+  end
+
+  return find_min_for_branch(0, initial_joltages, math.maxinteger)
+end
+
+local function part2(input_path)
+  local machines = parse_input(input_path)
+  local total = 0
+  for _, machine in pairs(machines) do
+    local min_presses = min_presses_to_joltage(machine)
+    print(min_presses)
+    total = total + min_presses
+  end
+  return total
+end
+
 assert(part1("./day10/example-input.txt") == 7)
 assert(part1("./day10/input.txt") == 547)
+print(part2("./day10/example-input.txt"))
