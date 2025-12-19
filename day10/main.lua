fae4ff985b22c351e39a69ae06cbd04aa2101f5d
Joe Barnett <joe.barnett@elliptic.co>
2025-12-19 17:43:31 +0000

    wip

--- day10/main.lua
+++ day10/main.lua
@@ -12,7 +12,7 @@ local function parse_buttons(buttons_str)
     local button = { list = {}, mask = 0 }
     for num_str in button_str:gmatch("%d+") do
       local num = tonumber(num_str)
-      table.insert(button.list, num)
+      table.insert(button.list, num + 1)
       button.mask = button.mask | 1 << num
     end
     table.insert(buttons, button)
@@ -101,7 +101,6 @@ local function serialise_num_list(list)
   return string.format("[ %s ]", table.concat(str_arr, ", "))
 end
 
-
 local function copy_list(list)
   local copy = {}
   for _, val in pairs(list) do
@@ -110,43 +109,67 @@ local function copy_list(list)
   return copy
 end
 
-local function take(list, n)
-  local chunk = {}
-  for _ = 1, math.min(#list, n) do
-    table.insert(chunk, table.remove(list, 1))
+local function includes(list, x)
+  for _, y in pairs(list) do
+    if x == y then
+      return true
+    end
   end
-  return chunk
+  return false
 end
 
 local function min_presses_to_joltage(machine)
   local function find_biggest_button_per_joltage()
-    local result = {}
-    for _, joltage in pairs(machine.joltages) do
+    local biggest_buttons = {}
+    for joltage_idx = 1, #machine.joltages do
       local biggest_button = nil
-      error("todo")
+      for _, button in pairs(machine.buttons) do
+        if includes(button.list, joltage_idx) then
+          if biggest_button == nil or #button.list > #biggest_button.list then
+            biggest_button = button
+          end
+        end
+      end
+      table.insert(biggest_buttons, biggest_button)
     end
-    return result
+    return biggest_buttons
   end
+
   local biggest_button_per_joltage = find_biggest_button_per_joltage()
+  for i, button in pairs(biggest_button_per_joltage) do
+    print(string.format("joltage_idx: %d, button: %s", i, serialise_num_list(button.list)))
+  end
 
   local function branch_lower_bound(remaining_joltages, current_press_count)
-    -- TODO - what we *should* be doing here is taking the approach as if we didn't
-    -- care about exceeding the joltage. Here, for each joltage, you would simply
-    -- choose the BIGGEST button that included that joltage, and press that as many
-    -- times as necessary to get that joltage to zero, decreasing the other joltages
-    -- affected by that button as a happy side effect.
-    local copy = copy_list(remaining_joltages)
-    table.sort(copy, function(a, b) return a > b end)
-    print(serialise_num_list(copy))
-    local lower_bound = 0
-    while #copy > 0 do
-      local chunk = take(copy, biggest_button_per_joltage)
-      if #chunk > 0 then
-        lower_bound = lower_bound + chunk[1]
+    local indent = ("  "):rep(current_press_count)
+    local remaining_joltages_copy = copy_list(remaining_joltages)
+    local remaining_presses_lower_bound = 0
+    while true do
+      print(string.format("%s remaining_joltages_copy:", indent))
+      print(string.format("%s %s", indent, serialise_num_list(remaining_joltages_copy)))
+      local max_remaining_joltage, max_remaining_joltage_idx = nil, nil
+      for joltage_idx = 1, #remaining_joltages_copy do
+        local remaining_joltage = remaining_joltages_copy[joltage_idx]
+        if remaining_joltage > 0 then
+          if max_remaining_joltage == nil or remaining_joltage > max_remaining_joltage then
+            max_remaining_joltage = remaining_joltage
+            max_remaining_joltage_idx = joltage_idx
+          end
+        end
+      end
+
+      if max_remaining_joltage == nil then
+        return remaining_presses_lower_bound + current_press_count
+      end
+
+      local biggest_button_affecting_most_remaining_joltage = biggest_button_per_joltage[max_remaining_joltage_idx]
+      -- print("biggest_button_affecting_most_remaining_joltage:")
+      -- print(serialise_num_list(biggest_button_affecting_most_remaining_joltage.list))
+      remaining_presses_lower_bound = remaining_presses_lower_bound + max_remaining_joltage
+      for _, joltage_idx in pairs(biggest_button_affecting_most_remaining_joltage.list) do
+        remaining_joltages_copy[joltage_idx] = remaining_joltages_copy[joltage_idx] - max_remaining_joltage
       end
     end
-    print(biggest_button_per_joltage, lower_bound)
-    return lower_bound + current_press_count
   end
 
   local function check_joltage_status(remaining_joltages)
@@ -163,34 +186,29 @@ local function min_presses_to_joltage(machine)
 
   local function find_min_for_branch(current_press_count, remaining_joltages, min_presses_to_target)
     local indent = ("  "):rep(current_press_count)
-    -- print(string.format("%scurrent_press_count: %d", indent, current_press_count))
-    -- print(string.format("%smin_presses_to_target: %d", indent, min_presses_to_target))
-    -- print(string.format("%sremaining_joltages: %s", indent, serialise_num_list(remaining_joltages)))
     local reached_target_joltages, exceeded_target_joltages = check_joltage_status(remaining_joltages)
 
     if reached_target_joltages then
-      -- print(string.format("%sreached target", indent))
+      print(string.format("%s reached target joltages", indent))
       return math.min(current_press_count, min_presses_to_target)
     end
 
     if exceeded_target_joltages then
-      -- print(string.format("%sexceeded joltage targets", indent))
+      print(string.format("%s exceeded target joltages", indent))
       return min_presses_to_target
     end
 
     if branch_lower_bound(remaining_joltages, current_press_count) >= min_presses_to_target then
-      -- print(string.format("%spruning by lower bound", indent))
+      print(string.format("%s never gonna work, abandoning branch", indent))
       return min_presses_to_target
     end
-
 
     for _, button in pairs(machine.buttons) do
       local next_press_count = current_press_count + 1
       local next_joltages = copy_list(remaining_joltages)
       for _, joltage_idx in pairs(button.list) do
-        next_joltages[joltage_idx + 1] = next_joltages[joltage_idx + 1] - 1
+        next_joltages[joltage_idx] = next_joltages[joltage_idx] - 1
       end
-      -- print(string.format("%snext_joltages: %s", indent, serialise_num_list(next_joltages)))
       min_presses_to_target = math.min(min_presses_to_target,
         find_min_for_branch(next_press_count, next_joltages, min_presses_to_target))
     end
@@ -206,7 +224,6 @@ local function part2(input_path)
   local total = 0
   for _, machine in pairs(machines) do
     local min_presses = min_presses_to_joltage(machine)
-    -- print(min_presses)
     total = total + min_presses
   end
   return total
@@ -215,4 +232,4 @@ end
 assert(part1("./day10/example-input.txt") == 7)
 assert(part1("./day10/input.txt") == 547)
 print(part2("./day10/example-input.txt"))
-print(part2("./day10/input.txt"))
+-- print(part2("./day10/input.txt"))
